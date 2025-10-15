673f38c63a51773c07226607356fd606a31ff0ff
Lenny <lennytiti3@gmail.com>
2025-10-15 16:50:53 +0800

    new homework

--- lenny_sum.py
+++ lenny_sum.py
@@ -1,11 +1,18 @@
 #
 # Lenny,2025/09/24
 # file : Lenny_sum.py
-# Short description of the task
+#Short description of the task
 #
 
-# 1. Input
+ # 1. Input
+order_values = [120,450,80,300,650]
+total_revenue = 0
 
-# 2. Process
+2. Process
+for order in order_values:
+    total_revenue += order
+    print(f"Processing order :${order}")
+
+    print(f"\nTotal Renevnue :${total_revenue}")
 
 # 3. Output
